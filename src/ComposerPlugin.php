34940c78271ebce28424602b2ca937b140dbc2a9
Marek Sterzik <marek.sterzik@gmail.com>
2023-10-17 22:58:13 +0200

    wip: extending composer plugin

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -5,14 +5,16 @@ namespace SPSOstrov\Runtime;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
 
-class ComposerPlugin implements PluginInterface
+class ComposerPlugin implements PluginInterface, EventSubscriberInterface
 {
+    private $composer;
+    private $io;
     public function activate(Composer $composer, IOInterface $io)
     {
-        echo "ACTIVATE!\n";
-        //$installer = new TemplateInstaller($io, $composer);
-        //$composer->getInstallationManager()->addInstaller($installer);
+        $this->composer = $composer;
+        $this->io = $io;
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
@@ -21,5 +23,18 @@ class ComposerPlugin implements PluginInterface
 
     public function uninstall(Composer $composer, IOInterface $io)
     {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'post-autoload-dump' => 'postAutoloadDump',
+        ];
+    }
+
+    public function postAutoloadDump($object = null)
+    {
+        echo "postAutoloadDump()\n";
+        var_dump($object);
     }
 }
