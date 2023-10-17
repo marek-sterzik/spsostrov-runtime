5089876b1651f8daec15ff46656131b0dfd057af
Marek Sterzik <marek.sterzik@gmail.com>
2023-10-18 00:32:20 +0200

    wip: composer plugin

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -10,6 +10,8 @@ use Composer\EventDispatcher\EventSubscriberInterface;
 
 class ComposerPlugin implements PluginInterface, EventSubscriberInterface
 {
+    const SELF_PACKAGE = "spsostrov/runtime";
+
     private $composer;
     private $io;
     public function activate(Composer $composer, IOInterface $io)
@@ -35,14 +37,19 @@ class ComposerPlugin implements PluginInterface, EventSubscriberInterface
 
     public function postAutoloadDump($object = null)
     {
-        $data = [];
+        $runtimeConfig = [];
         $rootDir = $this->canonizePath(InstalledVersions::getInstallPath("__root__"));
-        foreach (InstalledVersions::getInstalledPackagesByType('spsostrov-runtime') as $package) {
+        $packages = array_merge(
+            ['__root__'],
+            InstalledVersions::getInstalledPackagesByType('spsostrov-runtime'),
+            [self::SELF_PACKAGE]
+        );
+        foreach ($packages as $package) {
             $installPath = InstalledVersions::getInstallPath($package);
             $path = $this->canonizePath($installPath);
             $path = $this->stripPathPrefix($path, $rootDir);
             if ($path !== null) {
-                $data[$package] = $path;
+                $runtimeConfig[$package] = $this->loadDirsFromPackage($rootDir, $path, $package);
             } else {
                 $this->io->writeError([
                         sprintf(
@@ -52,7 +59,99 @@ class ComposerPlugin implements PluginInterface, EventSubscriberInterface
                 ]);
             }
         }
-        var_dump($rootDir, $data);
+        var_dump($rootDir, $runtimeConfig);
+    }
+
+    private function loadDirsFromPackage($rootDir, $packageDir, $package)
+    {
+        $config = $this->loadExtraFromComposerJson($rootDir, $packageDir, $package);
+        if (!array_key_exists('scripts-dir', $config)) {
+            $config['scripts-dir'] = 'scripts';
+        }
+        if (is_string($config['scripts-dir'])) {
+            $config['scripts-dir'] = $this->canonizePath(
+                $packageDir . "/" . $this->canonizeRelativePath($config['scripts-dir'])
+            );
+            if ($config['scripts-dir'] === null) {
+                $this->io->writeError([
+                        sprintf(
+                            "<warning>Package %s contains invalid scripts-dir</warning>",
+                            $package
+                        )
+                ]);
+            }
+        }
+        return $config;
+    }
+
+    private function canonizeRelativePath($path)
+    {
+        $path = ltrim('/', $path);
+        if ($path === '') {
+            return null;
+        }
+        $path = $this->canonizePath($path);
+        if ($path === '..' || substr($path, 0, 3) === '../') {
+            return null;
+        }
+        return $path;
+    }
+
+    private function loadExtraFromComposerJson($rootDir, $packageDir, $package)
+    {
+        $composerFile = sprintf("%s/%s/composer.json", $rootDir, $packageDir);
+        $content = @file_get_contents($composerFile);
+        if (!is_string($content)) {
+            return [];
+        }
+        $content = @json_decode($content, true);
+        if (!is_array($content)) {
+                $this->io->writeError([
+                        sprintf(
+                            "<warning>Package %s has broken composer.json</warning>",
+                            $package
+                        )
+                ]);
+                return [];
+        }
+        if (!isset($content['extra'])) {
+            return [];
+        }
+        if (!is_array($content['extra'])) {
+            $this->io->writeError([
+                    sprintf(
+                        "<warning>Package %s has broken extra field in composer.json</warning>",
+                        $package
+                    )
+            ]);
+            return [];
+        }
+        if (!isset($content['extra']['spsostrov-runtime'])) {
+            return [];
+        }
+        $config = $content['extra']['spsostrov-runtime']
+        if (!$this->validateExtraConfig($config)) {
+            $this->io->writeError([
+                    sprintf(
+                        "<warning>Package %s has broken runtime configuration in composer.json</warning>",
+                        $package
+                    )
+            ]);
+            return [];
+        }
+
+        return $config;
+    }
+
+    private function validateExtraConfig(&$config)
+    {
+        if (!is_array($config)) {
+            return false;
+        }
+        if (isset($config['scripts-dir']) && !is_string($config['scripts-dir'])) {
+            return false;
+        }
+        return true;
     }
 
     private function stripPathPrefix($path, $prefix)
