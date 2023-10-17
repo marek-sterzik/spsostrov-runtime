1914d167a0583b2e1df5bd03fb3c793c6c5b064e
Marek Sterzik <marek.sterzik@gmail.com>
2023-10-17 22:50:24 +0200

    update plugin

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -14,4 +14,12 @@ class ComposerPlugin implements PluginInterface
         //$installer = new TemplateInstaller($io, $composer);
         //$composer->getInstallationManager()->addInstaller($installer);
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 }
