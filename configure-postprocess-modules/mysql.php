64e4560b393a1a0e10c06e523c95223b8c6feaca
Marek Sterzik <marek.sterzik@gmail.com>
2023-11-06 01:47:34 +0100

    basic container control - start and stop

--- configure-postprocess-modules/mysql.php
+++ configure-postprocess-modules/mysql.php
@@ -4,4 +4,5 @@ use SPSOstrov\Runtime\Config;
 
 Config::section("MySQL configuration");
 
+Config::set("APP_DB_URL", "mysql://user:userpass@db:3306/thedatabase?serverVersion=5.7");
 Config::addComposeFile("docker-compose/mysql.yml");
